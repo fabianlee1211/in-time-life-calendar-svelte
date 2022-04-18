c6b35be3fd3feb5ba69a1be38c202140bd112dce
fabianlee1211 <chleefabian@gmail.com>
2022-04-18 18:01:26 +0800

    fix: store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -5,9 +5,19 @@ import { sub, format } from 'date-fns';
 const defaultDate = sub(new Date(), { days: 1 });
 export const defaultDateString = format(defaultDate, 'yyyy-LL-dd');
 
-export const birthDate = writable(
-  browser ? localStorage?.getItem('birthDate') : defaultDateString
-);
+const localStorageStore = (key, initial) => {
+  const stored = (browser && localStorage?.getItem(key)) || '';
+
+  if (!stored) {
+    browser && localStorage?.setItem(key, initial);
+  }
+
+  const store = writable(stored);
+
+  return store;
+};
+
+export const birthDate = localStorageStore('birthDate', '');
 
 birthDate.subscribe((value) => {
   browser && localStorage?.setItem('birthDate', value);
