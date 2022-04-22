b51980bb8582a9efcf79f09e3c88a030fe8a2230
fabianlee1211 <chleefabian@gmail.com>
2022-04-22 23:24:18 +0800

    feat: add about

---
+++ src/components/footer.svelte
@@ -0,0 +1,22 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
+<footer
+  class="flex flex-col sm:flex-row justify-between items-center max-w-screen-2xl px-6 lg:px-12 mx-auto py-8 space-y-4 sm:space-y-0"
+>
+  <p class="text-sm opacity-30">
+    {`© ${year} by Fabian Lee | In Time Life Calendar`}
+  </p>
+  <ul class="flex text-sm space-x-4">
+    <li class="hover:opacity-100 opacity-30 transition-opacity">
+      <a href="https://github.com/fabianlee1211/in-time-life-calendar">GitHub</a
+      >
+    </li>
+    <li class="hover:opacity-100 opacity-30 transition-opacity">
+      <a target="_blank" rel="noopener noreferrer" href="https://sugarfabby.com"
+        >Find Me</a
+      >
+    </li>
+  </ul>
+</footer>
